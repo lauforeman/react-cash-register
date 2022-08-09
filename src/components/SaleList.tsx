4e1badc876f7f8760e14a927c8fa10880d1ebd08
lauforeman <lauraforerom@hotmail.com>
2022-08-08 22:10:38 -0500

    Master Detaiol Prepared

--- src/components/SaleList.tsx
+++ src/components/SaleList.tsx
@@ -4,7 +4,11 @@ import { API } from '../config/contants';
 import { NavLink } from 'react-router-dom';
 import { Product } from './ProductList';
 
-export interface ProductSale{
+type Dict<TValue> = {
+    [key: string]: TValue
+}
+
+export interface ProductSale {
     productSaleId: number,
     productId: number,
     saleId: number,
@@ -24,18 +28,21 @@ export interface Sale {
 }
 
 export default function SaleList() {
-    const[sales, setSales] = React.useState<ReadonlyArray<Sale>>([]);
+    const [sales, setSales] = React.useState<ReadonlyArray<Sale>>([]);
+
+    const [showDetail, setShowDetail] = React.useState<Dict<boolean>>({});
 
     const getSales = React.useCallback(async () => {
         try {
             const response = await axios.get<ReadonlyArray<Sale>>(`${API}/Sale`)
             setSales(response.data);
+            setShowDetail(response.data.reduce((dict, sale) => ({ ...dict, [sale.saleId]: false }), {} as Dict<boolean>))
         } catch (error) {
-            
+
             console.error(error);
         }
-        
-    }, [setSales]);
+
+    }, [setSales, setShowDetail]);
 
     React.useEffect(() => {
         getSales();
@@ -44,34 +51,54 @@ export default function SaleList() {
     return (
         <div className='sale-list'>
             <NavLink to='/sales/sale/new'>
-                 <button>Add</button>
+                <button>Add</button>
             </NavLink>
             <table className='sale-table'>
                 <thead>
                     <tr>
                         <th>Id</th>
                         <th>Date</th>
-                        <th>Total</th>
                         <th>Is Loan</th>
                         <th>Apartament Number</th>
+                        <th>Total</th>
+                        <th>Payment</th>
+                        <th>Change</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {sales.length > 0 ? (
-                        sales.map((sale, index) => 
-                        (<tr key = {sale.saleId} className ={index % 2 === 1 ? 'odd' : 'even'}>
-                            <td>{sale.saleId}</td>
-                            <td>{sale.date}</td>
-                            <td>{sale.total}</td>
-                            <td>{sale.isLoan ? 'Yes' : 'No'}</td>
-                            <td>{sale.apartmentNumber}</td>
-                            <td>
-                                 <NavLink to={`/sales/sale/${sale.saleId}`}>
-                                     <button disabled={sale.payment > 0}>Edit</button>
-                                 </NavLink>
-                            </td>
-                        </tr>
+                        sales.map((sale, index) => (
+                            <React.Fragment>
+                                <tr key={sale.saleId} className={index % 2 === 1 ? 'odd' : 'even'}>
+                                    <td>
+                                        <button onClick={() =>
+                                            setShowDetail({ ...showDetail, [sale.saleId]: !showDetail[sale.saleId]})
+                                            }>{showDetail[sale.saleId] ? '↓' : '→'}</button>
+                                        {sale.saleId}
+                                    </td>
+                                    <td>{sale.date}</td>
+                                    <td>{sale.isLoan ? 'Yes' : 'No'}</td>
+                                    <td>{sale.apartmentNumber}</td>
+                                    <td>{sale.total}</td>
+                                    <td>{sale.payment}</td>
+                                    <td>{sale.payment > 0 ? sale.payment - sale.payment : 0}</td>
+                                    <td>
+                                        <NavLink to={`/sales/sale/${sale.saleId}`}>
+                                            <button disabled={sale.payment > 0}>Edit</button>
+                                        </NavLink>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    {
+                                        showDetail[sale.saleId] && (
+                                            <tr>
+                                                <td colSpan={8}> Esto es un detalle</td>
+                                            </tr>
+                                        )
+                                    }
+                                </tr>
+                            </React.Fragment>
                         ))
                     ) : (
                         <tr>
